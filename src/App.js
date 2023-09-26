b09487f8d2759a5e0d7f8f9246005c81216a1280
Vanderlei Junior <102929339+Vanderlei94@users.noreply.github.com>
2023-09-26 16:12:40 +0000

    feat(game): implement game finished state

--- src/App.js
+++ src/App.js
@@ -51,16 +51,18 @@ function Board({xIsNext, squares, onPlay}) {
   );
 }
 export default function Game(){
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay (nextSquares){
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove +1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -1);
   }
 function jumpTo(nextMove){
-  //TODO
+  setCurrentMove(nextMove);
 }
 const moves = history.map((squares, move)=> {
   let description;
@@ -70,8 +72,8 @@ const moves = history.map((squares, move)=> {
     description = 'Go to Game start';
   }
   return (
-    <li> 
-      <button onClick={() => jumpTo(move)}>description</button>
+    <li key={move}> 
+      <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
   );
 });
